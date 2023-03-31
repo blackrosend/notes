7e7a1a48b353881ea943796724de8fb1a112d4fd
blackrosend <83564331+blackrosend@users.noreply.github.com>
2023-04-01 05:02:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser')
 
-const app = express();
-app.set('view engine', 'ejs');
 
-const express = require('express')
-const app = express()
-app.set('view engine', 'ejs')
+
+
+
+
+
+
+
   
-app.get("/", (req, res) => {
-    res.render("home", { variableName: "Hello Geeks!" })
-})
+
+
+
   
-app.listen(3000, (req, res) => {
-    console.log("App is running on port 3000")
-})
+
+
+
 
 
 const express = require('express')
